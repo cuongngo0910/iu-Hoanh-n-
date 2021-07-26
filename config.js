b6051f8308c0ad878f3efab896ac96878e7e25c4
cuongngo0910 <87963341+cuongngo0910@users.noreply.github.com>
2021-07-26 13:50:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'he lu Hoanh à!',
+    introTitle: 'he lu Kiều Anhh ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao cậu cứ đi nhầm đường
     Lạc vào tim tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn còn thích cậu<33',
-    btnNo: 'Nằm mơ à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho bé cường <3',
+    title: 'Kiều Anh còn giận Cường nữa à hic',
+    desc: 'Cường biết lỗi rùi mà ',
+    btnYes: 'Không có mà',
+    btnNo: 'Biến mẹ đi , hic :3',
+    question:' Đừng ghét tớ nữa nha <3',
+    btnReply: 'Gửi cho bé Cường <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'tớ biết mà 🥰. Yêu hoanh nhiều nhiều 😘😘',
-    messDesc: 'bắt đầu từ giờ hẹn hò luôn nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Tớ biết mà 🥰. Yêu Kiều Anh nhất đời 😘😘',
+    messDesc: 'Coi xong rùi đừng chửi Cường nha.',
+    btnAccept: 'Yêu Kiều Anh nè <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
